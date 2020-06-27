9e381f26fd447938452300ac5f5633609bb4f3bf
Vladimir Ruzhelovich <Ruzhelovich.Vladimir@gmail.com>
2020-06-27 22:56:52 +0600

    Lesson-5 7-t задание

--- lesson-5/task_7.py
+++ lesson-5/task_7.py
@@ -16,3 +16,41 @@
     [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
     Подсказка: использовать менеджер контекста.
 """
+
+from re import sub
+import json
+
+FILENAME = "text_7.txt"
+JSON_FILENAME = "text_7.json"
+RESULT = [{}, {}]
+PROFIT_SUM = 0
+PROFIT_INX = 0
+
+try:
+    with open(file=FILENAME, mode="r", encoding="utf-8") as f:
+        while True:
+            STR = f.readline()[:-1]
+            if len(STR) == 0:
+                break
+            # Удаляю лишние проблелы
+            STR_TEMP = sub(r"\s+", " ", STR)
+            # Удаляю лишние проблелы начале строки
+            STR_TEMP = sub(r"^\s+", "", STR_TEMP)
+            # Удаляю лишние проблелы в конце строки
+            STR_TEMP = sub(r"\s+$", "", STR_TEMP)
+            LST = STR_TEMP.split(" ")
+            SUBJECT = LST[0]
+            PROFIT = float(LST[2]) - float(LST[3])
+            if PROFIT >= 0:
+                PROFIT_SUM += PROFIT
+                PROFIT_INX += 1
+            RESULT[0].update({SUBJECT: PROFIT})
+    RESULT[1].update({"average_profit": PROFIT_SUM / PROFIT_INX})
+    with open(JSON_FILENAME, mode="w", encoding="utf-8") as f:
+        json.dump(RESULT, f, indent=4, ensure_ascii=False)
+except IOError:
+    print("Ошибка достапа к файлу")
+except ValueError:
+    print("Строка не является числом")
+else:
+    print(f"Ок.")
